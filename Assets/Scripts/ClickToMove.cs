ca755f64edd21b6eba729f3bc249b1139148d01a
gijsoman <gijsbakker1@hotmail.com>
2018-04-20 11:19:55 +0200

    First Upload
    This upload contains:
    
    - Basic character movement
    - UnityChan (Ugh i will change her out for the standard unity character)
    - Following camear (very basic)

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour {
+
+    public float Speed;
+    public CharacterController CharacterControl;
+    public Animator CharacterAnimator;
+
+    private Vector3 position;
+
+	void Start ()
+    {
+        position = transform.position;
+	}
+	
+	private void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //Locate where the player clicks on the terrain.
+            LocatePosition();
+        }
+
+        //Move the player to the position
+        MoveToPosition();
+	}
+
+    //Will locate the position on the terrain where the user clicked
+    private void LocatePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+   
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            Debug.Log(position);
+        }
+    }
+
+    private void MoveToPosition()
+    {
+        if (Vector3.Distance(transform.position, position) > 0.6f)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(position - transform.position);
+
+            newRotation.x = 0f;
+            newRotation.z = 0f;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+            CharacterControl.SimpleMove(transform.forward * Speed);
+
+            CharacterAnimator.SetFloat("Speed", Speed);
+        }
+        else
+        {
+            CharacterAnimator.SetFloat("Speed", 0);
+        }
+    }
+}
